c5aab21b3e9e0e5fcf31fba31a328a5f662918e4
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-03-15 23:30:02 +0100

    fix: invalid CSS / vite 8

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       output: {
         assetFileNames: "assets/[name].[hash][extname]",
         chunkFileNames: "assets/[name].[hash].js",
