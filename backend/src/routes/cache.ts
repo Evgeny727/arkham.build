dae7e891b7b0567d1e3538c0ce86921a6131de25
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-04-24 08:03:59 +0200

    fix: use unix timestamp format

--- backend/src/routes/cache.ts
+++ backend/src/routes/cache.ts
@@ -89,7 +89,7 @@ async function cachedResponse<T>(
 ) {
   const db = c.get("db");
   const version = await getVersionForLocale(db, options.locale);
-  const etag = `${options.resource}:${options.locale}:${version.cards_updated_at}:${version.translation_updated_at}`;
+  const etag = `${options.resource}:${options.locale}:${version.cards_updated_at.valueOf()}:${version.translation_updated_at.valueOf()}`;
 
   applyCacheHeaders(c, { etag, resource: options.resource });
 
