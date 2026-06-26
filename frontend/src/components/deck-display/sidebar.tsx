f08cd36bcce1334484fbeb78f223bcb09678ad18
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-06-26 21:12:55 +0200

    fix: consider old shares when rendering deck access section

--- frontend/src/components/deck-display/sidebar.tsx
+++ frontend/src/components/deck-display/sidebar.tsx
@@ -30,7 +30,6 @@ import { useToast } from "@/components/ui/toast.hooks";
 import { UpgradeModal } from "@/pages/deck-view/upgrade-modal";
 import { useImportSharedDeckMutation } from "@/queries/mutations/decks";
 import { useStore } from "@/store";
-import { isSyncedStorageProvider } from "@/store/lib/sync";
 import type { ResolvedDeck } from "@/store/lib/types";
 import { selectDeckCreateStorageProviderOptions } from "@/store/selectors/deck-create";
 import type { History } from "@/store/selectors/decks";
@@ -478,7 +477,7 @@ function Sharing(props: {
     selectDeckCreateStorageProviderOptions,
   );
 
-  const isSynced = isSyncedStorageProvider(deck.source);
+  const isSynced = deck.source !== "local";
   const availableUploadProviders = uploadProviders.filter((provider) =>
     storageProviderOptions.some((option) => option.value === provider),
   );
