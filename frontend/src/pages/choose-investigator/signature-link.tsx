04e1e112530dd91f3fcc9f994ca63f3c4b429c0a
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-07-12 09:09:57 +0200

    refactor: reuse result tag for signature links

--- frontend/src/pages/choose-investigator/signature-link.tsx
+++ frontend/src/pages/choose-investigator/signature-link.tsx
@@ -1,12 +1,6 @@
 import type { Card } from "@arkham-build/shared";
-import { FloatingPortal, shift } from "@floating-ui/react";
-import { useCallback } from "react";
-import { CardTooltip } from "@/components/card-tooltip/card-tooltip";
-import { useRestingTooltip } from "@/components/ui/tooltip.hooks";
-import { useStore } from "@/store";
-import { displayAttribute } from "@/utils/card-utils";
-import { FLOATING_PORTAL_ID } from "@/utils/constants";
-import css from "./choose-investigator.module.css";
+import { ListCard } from "@/components/list-card/list-card";
+import { ResultTag } from "@/components/ui/combobox/combobox-results";
 
 type Props = {
   card: Card;
@@ -15,39 +9,16 @@ type Props = {
 export function SignatureLink(props: Props) {
   const { card } = props;
 
-  const {
-    closeTooltip,
-    refs,
-    referenceProps,
-    isMounted,
-    floatingStyles,
-    transitionStyles,
-  } = useRestingTooltip({
-    middleware: [shift({ padding: 5 })],
-    placement: "right",
-  });
-
-  const openCardModal = useStore((state) => state.openCardModal);
-
-  const openModal = useCallback(() => {
-    closeTooltip();
-    openCardModal(card.code);
-  }, [card.code, closeTooltip, openCardModal]);
-
   return (
-    <li className={css["signature"]} key={card.code}>
-      <button ref={refs.setReference} {...referenceProps} onClick={openModal}>
-        {displayAttribute(card, "name")}
-      </button>
-      {isMounted && (
-        <FloatingPortal id={FLOATING_PORTAL_ID}>
-          <div ref={refs.setFloating} style={floatingStyles}>
-            <div style={transitionStyles}>
-              <CardTooltip code={card.code} />
-            </div>
-          </div>
-        </FloatingPortal>
-      )}
-    </li>
+    <ResultTag>
+      <ListCard
+        card={card}
+        cardLevelDisplay="icon-only"
+        cardShowCollectionNumber
+        omitBorders
+        omitThumbnail
+        size="xs"
+      />
+    </ResultTag>
   );
 }
